b3af43496fcc7c185af0dd81e310fb250abd5a17
Your Name <20110568@student.hcmute.edu.vn>
2023-12-16 22:52:05 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,9 @@ async function bootstrap() {
         .addTag('tlcn')
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document,{
+        customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    });
 
     app.enableCors({
         origin: [
